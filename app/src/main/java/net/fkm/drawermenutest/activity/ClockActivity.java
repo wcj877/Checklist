94f970604f711f3deff8049831f7b92fff51440c
86133 <2694825792@qq.com>
2021-01-06 19:54:21 +0800

    优化界面

--- app/src/main/java/net/fkm/drawermenutest/activity/ClockActivity.java
+++ app/src/main/java/net/fkm/drawermenutest/activity/ClockActivity.java
@@ -47,7 +47,7 @@ public class ClockActivity extends BaseActivity {
         //获取自定义布局
         View inflate = LayoutInflater.from(this).inflate(R.layout.dialog_alarm_layout, null);//获取时钟的布局
         TextView textView = inflate.findViewById(R.id.tv_event);//获取提示内容的视图
-        textView.setText(String.format(getString(R.string.clock_event_msg_template), list.getDescribe()));//设置提示的内容
+        textView.setText(String.format(getString(R.string.clock_event_msg_template), list.getListTitle()));//设置提示的内容
         Button btnConfirm = inflate.findViewById(R.id.btn_confirm);//获取确认视图
         final AlertDialog alertDialog = AlertDialogUtil.showDialog(this, inflate);//设置inflate为自定义对话框
         btnConfirm.setOnClickListener(new View.OnClickListener() {//点击确认关闭对话框
